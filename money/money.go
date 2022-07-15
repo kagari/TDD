7e83affd12ad48bb16fe73b12318dd47a64ec075
kagari <e175766@ie.u-ryukyu.ac.jp>
2022-07-15 16:12:58 +0900

    [green] いい感じにできた

--- money/money.go
+++ money/money.go
@@ -1,5 +1,9 @@
 package money
 
+import (
+        "reflect"
+)
+
 type Money struct {
 	amount int
 }
@@ -17,5 +21,8 @@ func (m *Money) equals(object *Money) bool {
 }
 
 func equals(object Equallity, amount HasMoney) bool {
+        if reflect.TypeOf(object) != reflect.TypeOf(amount) {
+		return false
+	}
 	return object.mequals(amount.get_money())
 }
