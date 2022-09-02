d9c105bc781403c4e779dae4922ead42e526380d
kagari <e175766@ie.u-ryukyu.ac.jp>
2022-09-02 14:49:23 +0900

    [green] test TestCase

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -19,7 +19,12 @@ class WasRun(TestCase):
         self.wasRun = 1
 
 
-test =  WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test =  WasRun("testMethod")
+        assert not test.wasRun
+        test.run()
+        assert test.wasRun
+
+
+TestCaseTest("testRunning").run()
