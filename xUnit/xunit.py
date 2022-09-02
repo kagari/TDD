1eebaf4904ff08e9e1cf1935be72623c440ba94c
soto <soto.1999.u@gmail.com>
2022-09-02 15:41:01 +0900

    DONE Chapter20

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -8,33 +8,31 @@ class TestCase:
     def setUp(self) -> None:
         pass
 
+    def tearDown(self) -> None:
+        pass
+
     def run(self) -> None:
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 
 class WasRun(TestCase):
     def setUp(self) -> None:
-        self.wasRun: Optional[int] = None
-        self.wasSetUp = 1
+        self.log = "setUp "
 
     def testMethod(self) -> None:
-        self.wasRun = 1
+        self.log = self.log + "testMethod "
+
+    def tearDown(self) -> None:
+        self.log = self.log + "tearDown "
 
 
 class TestCaseTest(TestCase):
-    def setUp(self) -> None:
-        self.test = WasRun("testMethod")
+    def testTemplateMethod(self) -> None:
+        test = WasRun("testMethod")
+        test.run()
+        assert "setUp testMethod tearDown " == test.log
 
-    def testRunning(self) -> None:
-        self.test.run()
-        assert self.test.wasRun
-
-    def testSetUp(self) -> None:
-        self.test.run()
-        assert self.test.wasSetUp
-
-
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
