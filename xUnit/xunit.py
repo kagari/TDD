54313a56bde9af8326401a2cec9a55f5008afa22
kagari <e175766@ie.u-ryukyu.ac.jp>
2022-09-02 14:36:28 +0900

    [green] git add xunit.py

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -1,5 +1,10 @@
 class WasRun:
-    pass
+    def __init__(self, name: str) -> None:
+        self.wasRun = None
+
+    def testMethod(self) -> None:
+        self.wasRun = 1
+
 
 test =  WasRun("testMethod")
 print(test.wasRun)
